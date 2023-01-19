ce7dde53863759c2dd4b38ce23815edc49834e26
Jesse Braham <jesse@beta7.io>
2023-01-19 11:47:23 -0800

    Write an xtask subcommand to run the `bindgen` generation

--- esp-ieee802154/mkbindings.bat
+++
@@ -1,3 +0,0 @@
-set INCL=%HOMEPATH%\.espressif\tools\riscv32-esp-elf\esp-2021r2-8.4.0\riscv32-esp-elf\riscv32-esp-elf\include\
-set OPTS=--no-derive-debug --raw-line "#![allow(non_camel_case_types,non_snake_case,non_upper_case_globals,dead_code,improper_ctypes)]" --use-core --ctypes-prefix "crate::binary::c_types" --no-layout-tests 
-bindgen %OPTS% include\include.h > src\binary\include_esp32h4.rs -- -I./headers/ -I%INCL% -I./include/ -DCONFIG_IDF_TARGET_ESP32H4 -I./headers/esp32h4/
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -2,31 +2,62 @@ use std::{
     env,
     fs::{self, File},
     io::Write,
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
 use anyhow::Result;
-use clap::Parser;
+use bindgen::Builder;
+use clap::{Parser, Subcommand, ValueEnum};
+use directories::BaseDirs;
 use log::{info, LevelFilter};
+use strum::Display;
 use svd2rust::{generate::device::render, load_from, Config, Target};
 use svdtools::patch::process_file;
 use xshell::{cmd, Shell};
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display, ValueEnum)]
+#[strum(serialize_all = "lowercase")]
+enum Chip {
+    /// ESP32-H4
+    Esp32h4,
+}
+
 #[derive(Debug, Parser)]
-struct Opts {}
+struct Cli {
+    #[clap(subcommand)]
+    subcommand: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Generate the Register Access Layer (RAL)
+    Ral,
+    /// Generate the binary includes using `bindgen`
+    Includes {
+        #[clap(value_enum)]
+        chip: Chip,
+    },
+}
 
 fn main() -> Result<()> {
     env_logger::Builder::new()
         .filter_module("xtask", LevelFilter::Info)
         .init();
 
-    let _opts = Opts::parse();
+    let args = Cli::parse().subcommand;
 
     // The directory containing the cargo manifest for the 'xtask' package is a
     // subdirectory within the cargo workspace.
     let workspace = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let workspace = workspace.parent().unwrap().canonicalize()?;
 
+    match args {
+        Commands::Ral => generate_register_access_layer(&workspace),
+        Commands::Includes { chip } => generate_binary_includes(&workspace, chip),
+    }
+}
+
+fn generate_register_access_layer(workspace: &Path) -> Result<()> {
     let svd_path = workspace.join("svd");
     let svd_file = svd_path.join("ieee802154.svd");
     let out_dir = workspace.join("esp-ieee802154").join("src").join("ral");
@@ -81,6 +112,66 @@ fn main() -> Result<()> {
     cmd!(sh, "rustfmt {out_dir}/mod.rs").quiet().run()?;
 
     info!("All done.");
+
+    Ok(())
+}
+
+fn generate_binary_includes(workspace: &Path, chip: Chip) -> Result<()> {
+    let base_dirs = BaseDirs::new().unwrap();
+    let home_path = base_dirs.home_dir();
+    let ieee_path = workspace.join("esp-ieee802154");
+
+    info!("generating bindings with bindgen");
+    let bindings = Builder::default()
+        .clang_args([
+            format!("-I{}", ieee_path.join("headers").display()),
+            format!(
+                "-I{}",
+                ieee_path.join("headers").join(chip.to_string()).display()
+            ),
+            format!("-I{}", ieee_path.join("include").display()),
+            format!(
+                "-I{}",
+                home_path
+                    .join(".espressif")
+                    .join("tools")
+                    .join("riscv32-esp-elf")
+                    .join("esp-2021r2-8.4.0")
+                    .join("riscv32-esp-elf")
+                    .join("riscv32-esp-elf")
+                    .join("include")
+                    .display()
+            ),
+            format!("-DCONFIG_IDF_TARGET_{}", chip.to_string().to_uppercase()),
+        ])
+        .ctypes_prefix("crate::binary::c_types")
+        .derive_debug(false)
+        .header(
+            ieee_path
+                .join("include")
+                .join("include.h")
+                .to_string_lossy(),
+        )
+        .layout_tests(false)
+        .raw_line("#![allow(dead_code)]")
+        .raw_line("#![allow(improper_ctypes)]")
+        .raw_line("#![allow(non_camel_case_types)]")
+        .raw_line("#![allow(non_snake_case)]")
+        .raw_line("#![allow(non_upper_case_globals)]")
+        .use_core()
+        .generate()?;
+
+    let bindings_path = ieee_path
+        .join("src")
+        .join("binary")
+        .join("include")
+        .join(format!("{chip}.rs"));
+
+    bindings.write_to_file(&bindings_path)?;
+    info!(
+        "generated bindings written to '{}'",
+        bindings_path.display()
+    );
 
     Ok(())
 }
