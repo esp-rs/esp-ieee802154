8d084f50467e10023920227248d2c8dc251ee060
bjoernQ <bjoern.quentin@mobile-j.de>
2023-03-09 16:41:13 +0100

    WIP still wondering about WIFI_BB

--- esp-ieee802154/examples/simple.rs
+++ esp-ieee802154/examples/simple.rs
@@ -42,49 +42,35 @@ fn main() -> ! {
     println!("Enabled");
 
     unsafe {
-        const DR_REG_PLIC_MX_BASE: u32 = 0x20001000;
-        const PLIC_MXINT0_PRI_REG: u32 = DR_REG_PLIC_MX_BASE + 0x10;
-        println!(
-            "interrupt WIFI BB {}",
-            ((PLIC_MXINT0_PRI_REG + 3 * 4) as *mut u32).read_volatile()
-        );
-        ((PLIC_MXINT0_PRI_REG + 3 * 4) as *mut u32).write_volatile(0);
-
-        // try to disable the WIFI_BB interrupt
-        println!(
-            "interrupt WIFI BB {}",
-            ((0x60010000 + 3 * 4) as *mut u32).read_volatile()
-        );
-        ((0x60010000 + 3 * 4) as *mut u32).write_volatile(0);
+        // map WIFI_BB to the masked interrupt 31 to disable it
+        // not doing that will freeze up the code (since it's endless calling the DefaultHandler)
+        // uncommenting WIFI_BB at the bottom makes it work (but the ISR is called)
+        (0x60010000 as *mut u32).offset(3).write_volatile(31);
     }
-    // esp32c6_hal::interrupt::enable(
-    //     esp32c6_hal::peripherals::Interrupt::WIFI_BB,
-    //     esp32c6_hal::interrupt::Priority::Priority1,
-    // );
 
     let mut delay = Delay::new(&clocks);
     delay.delay_ms(10u32);
 
-    // set_channel(11);
-    // loop {
-    //     // data need to be in SRAM
-    //     let mut frame: [u8; 55] = [
-    //         54, 0x41, 0x88, 0x44, 0xff, 0x01, 0xff, 0xff, 0x00, 0x00, //
-    //         b'H', b'e', b'l', b'l', b'o', b' ', b'W', b'o', b'r', b'l', b'd', b'!', b'H', b'e',
-    //         b'l', b'l', b'o', b' ', b'E', b'S', b'P', b'3', b'2', b'-', b'C', b'6', b'!', b' ',
-    //         b'8', b'0', b'2', b'.', b'1', b'5', b'.', b'4', b' ', b't', b'e', b's', b't', b'!',
-    //         b'!', //
-    //         0x00, 0x00,
-    //     ];
-    //     println!("FRAME: {:02x?}", frame);
-    //     println!("frame addr = {:x}", frame.as_ptr() as u32);
-    //     println!("call transmit");
-    //     ieee802154_transmit(frame.as_ptr() as *const u8, false);
-    //     println!("called transmit");
-    //     println!();
+    set_channel(11);
+    loop {
+        // data need to be in SRAM
+        let mut frame: [u8; 55] = [
+            54, 0x41, 0x88, 0x44, 0xff, 0x01, 0xff, 0xff, 0x00, 0x00, //
+            b'H', b'e', b'l', b'l', b'o', b' ', b'W', b'o', b'r', b'l', b'd', b'!', b'H', b'e',
+            b'l', b'l', b'o', b' ', b'E', b'S', b'P', b'3', b'2', b'-', b'C', b'6', b'!', b' ',
+            b'8', b'0', b'2', b'.', b'1', b'5', b'.', b'4', b' ', b't', b'e', b's', b't', b'!',
+            b'!', //
+            0x00, 0x00,
+        ];
+        println!("FRAME: {:02x?}", frame);
+        println!("frame addr = {:x}", frame.as_ptr() as u32);
+        println!("call transmit");
+        ieee802154_transmit(frame.as_ptr() as *const u8, false);
+        println!("called transmit");
+        println!();
 
-    //     delay.delay_ms(1000u32);
-    // }
+        delay.delay_ms(1000u32);
+    }
 
     set_channel(15);
     // set_promiscuous(false);
@@ -237,7 +223,7 @@ extern "C" fn rtc_clk_xtal_freq_get() -> i32 {
     0
 }
 
-#[interrupt]
-fn WIFI_BB() {
-    //    println!("WIFI BB interrupt");
-}
+// #[interrupt]
+// fn WIFI_BB() {
+//     println!("WIFI BB interrupt");
+// }
