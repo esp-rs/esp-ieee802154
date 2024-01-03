6b6095492750b620801e92109c4179bed10af66b
Jesse Braham <jesse@beta7.io>
2024-01-03 11:29:33 -0800

    Regenerate include and ral modules

--- esp-ieee802154/src/hal/mod.rs
+++ esp-ieee802154/src/hal/mod.rs
@@ -2,6 +2,7 @@ use core::ops::{BitAnd, BitOr};
 
 use crate::pib::CcaMode;
 
+#[allow(non_camel_case_types, unused)]
 #[cfg_attr(feature = "esp32c6", path = "ral/esp32c6.rs")]
 #[cfg_attr(feature = "esp32h2", path = "ral/esp32h2.rs")]
 mod ral;
@@ -9,18 +10,18 @@ mod ral;
 #[allow(unused)]
 #[derive(Debug, Clone, Copy)]
 pub(crate) enum Event {
-    TxDone = 1 << 0,
-    RxDone = 1 << 1,
-    AckTxDone = 1 << 2,
-    AckRxDone = 1 << 3,
-    RxAbort = 1 << 4,
-    TxAbort = 1 << 5,
-    EdDone = 1 << 6,
-    Timer0Overflow = 1 << 8,
-    Timer1Overflow = 1 << 9,
+    TxDone          = 1 << 0,
+    RxDone          = 1 << 1,
+    AckTxDone       = 1 << 2,
+    AckRxDone       = 1 << 3,
+    RxAbort         = 1 << 4,
+    TxAbort         = 1 << 5,
+    EdDone          = 1 << 6,
+    Timer0Overflow  = 1 << 8,
+    Timer1Overflow  = 1 << 9,
     ClockCountMatch = 1 << 10,
-    TxSfdDone = 1 << 11,
-    RxSfdDone = 1 << 12,
+    TxSfdDone       = 1 << 11,
+    RxSfdDone       = 1 << 12,
 }
 
 impl Event {
@@ -56,21 +57,21 @@ impl BitOr<Event> for u16 {
 #[allow(unused)]
 #[derive(Debug, Clone, Copy)]
 pub(crate) enum TxAbortReason {
-    RxAckStop = 1,
+    RxAckStop       = 1,
     RxAckSfdTimeout = 2,
-    RxAckCrcError = 3,
+    RxAckCrcError   = 3,
     RxAckInvalidLen = 4,
     RxAckFilterFail = 5,
-    RxAckNoRss = 6,
-    RxAckCoexBreak = 7,
+    RxAckNoRss      = 6,
+    RxAckCoexBreak  = 7,
     RxAckTypeNotAck = 8,
-    RxAckRestart = 9,
-    RxAckTimeout = 16,
-    TxStop = 17,
-    TxCoexBreak = 18,
+    RxAckRestart    = 9,
+    RxAckTimeout    = 16,
+    TxStop          = 17,
+    TxCoexBreak     = 18,
     TxSecurityError = 19,
-    CcaFailed = 24,
-    CcaBusy = 25,
+    CcaFailed       = 24,
+    CcaBusy         = 25,
 }
 
 impl TxAbortReason {
@@ -98,22 +99,22 @@ impl BitOr<TxAbortReason> for u32 {
 #[allow(unused)]
 #[derive(Debug, Clone, Copy)]
 pub(crate) enum RxAbortReason {
-    RxStop = 1,
-    SfdTimeout = 2,
-    CrcError = 3,
-    InvalidLen = 4,
-    FilterFail = 5,
-    NoRss = 6,
-    CoexBreak = 7,
-    UnexpectedAck = 8,
-    RxRestart = 9,
-    TxAckTimeout = 16,
-    TxAckStop = 17,
-    TxAckCoexBreak = 18,
+    RxStop              = 1,
+    SfdTimeout          = 2,
+    CrcError            = 3,
+    InvalidLen          = 4,
+    FilterFail          = 5,
+    NoRss               = 6,
+    CoexBreak           = 7,
+    UnexpectedAck       = 8,
+    RxRestart           = 9,
+    TxAckTimeout        = 16,
+    TxAckStop           = 17,
+    TxAckCoexBreak      = 18,
     EnhackSecurityError = 19,
-    EdAbort = 24,
-    EdStop = 25,
-    EdCoexReject = 26,
+    EdAbort             = 24,
+    EdStop              = 25,
+    EdCoexReject        = 26,
 }
 
 impl RxAbortReason {
@@ -148,18 +149,18 @@ pub(crate) enum EdSampleMode {
 #[allow(unused)]
 #[derive(Debug, Clone, Copy)]
 pub(crate) enum Command {
-    TxStart = 0x41,
-    RxStart = 0x42,
-    CcaTxStart = 0x43,
-    EdStart = 0x44,
-    Stop = 0x45,
-    DtmTxStart = 0x46,
-    DtmRxStart = 0x47,
-    DtmStop = 0x48,
+    TxStart     = 0x41,
+    RxStart     = 0x42,
+    CcaTxStart  = 0x43,
+    EdStart     = 0x44,
+    Stop        = 0x45,
+    DtmTxStart  = 0x46,
+    DtmRxStart  = 0x47,
+    DtmStop     = 0x48,
     Timer0Start = 0x4C,
-    Timer0Stop = 0x4D,
+    Timer0Stop  = 0x4D,
     Timer1Start = 0x4E,
-    Timer1Stop = 0x4F,
+    Timer1Stop  = 0x4F,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -183,34 +184,39 @@ impl From<usize> for MultipanIndex {
 }
 
 #[inline(always)]
+fn ieee802154() -> &'static self::ral::ieee802154::RegisterBlock {
+    unsafe { &*self::ral::IEEE802154::PTR }
+}
+
+#[inline(always)]
 pub(crate) fn mac_date() -> u32 {
-    ieee802154().mac_date.read().bits()
+    ieee802154().mac_date().read().bits()
 }
 
 #[inline(always)]
 pub(crate) fn set_rx_on_delay(delay: u16) {
     ieee802154()
-        .rxon_delay
+        .rxon_delay()
         .modify(|_, w| w.rxon_delay().variant(delay));
 }
 
 #[inline(always)]
 pub(crate) fn enable_events(events: u16) {
     ieee802154()
-        .event_en
+        .event_en()
         .modify(|r, w| w.event_en().variant(r.event_en().bits() | events));
 }
 
 #[inline(always)]
 pub(crate) fn disable_events(events: u16) {
     ieee802154()
-        .event_en
+        .event_en()
         .modify(|r, w| w.event_en().variant(r.event_en().bits() & !events));
 }
 
 #[inline(always)]
 pub(crate) fn enable_tx_abort_events(events: u32) {
-    ieee802154().tx_abort_interrupt_control.modify(|r, w| {
+    ieee802154().tx_abort_interrupt_control().modify(|r, w| {
         w.tx_abort_interrupt_control()
             .variant(r.tx_abort_interrupt_control().bits() | events)
     });
@@ -218,7 +224,7 @@ pub(crate) fn enable_tx_abort_events(events: u32) {
 
 #[inline(always)]
 pub(crate) fn enable_rx_abort_events(events: u32) {
-    ieee802154().rx_abort_intr_ctrl.modify(|r, w| {
+    ieee802154().rx_abort_intr_ctrl().modify(|r, w| {
         w.rx_abort_intr_ctrl()
             .variant(r.rx_abort_intr_ctrl().bits() | events)
     });
@@ -227,38 +233,38 @@ pub(crate) fn enable_rx_abort_events(events: u32) {
 #[inline(always)]
 pub(crate) fn set_ed_sample_mode(ed_sample_mode: EdSampleMode) {
     ieee802154()
-        .ed_scan_cfg
+        .ed_scan_cfg()
         .modify(|_, w| w.ed_sample_mode().variant(ed_sample_mode as u8));
 }
 
 #[inline(always)]
 pub(crate) fn set_tx_addr(addr: *const u8) {
     ieee802154()
-        .txdma_addr
+        .txdma_addr()
         .modify(|_, w| w.txdma_addr().variant(addr as u32));
 }
 
 #[inline(always)]
 pub(crate) fn set_cmd(cmd: Command) {
     ieee802154()
-        .command
+        .command()
         .modify(|_, w| w.opcode().variant(cmd as u8))
 }
 
 #[inline(always)]
 pub(crate) fn set_freq(freq: u8) {
-    ieee802154().channel.modify(|_, w| w.hop().variant(freq));
+    ieee802154().channel().modify(|_, w| w.hop().variant(freq));
 }
 
 #[inline(always)]
 pub(crate) fn get_freq() -> u8 {
-    ieee802154().channel.read().hop().bits()
+    ieee802154().channel().read().hop().bits()
 }
 
 #[inline(always)]
 pub(crate) fn set_power(power: u8) {
     ieee802154()
-        .tx_power
+        .tx_power()
         .modify(|_, w| w.tx_power().variant(power));
 }
 
@@ -266,14 +272,17 @@ pub(crate) fn set_power(power: u8) {
 pub(crate) fn set_multipan_enable_mask(mask: u8) {
     // apparently the REGS are garbage and the struct is right?
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .modify(|r, w| unsafe { w.bits(r.bits() & !(0b1111 << 29) | (mask as u32) << 29) })
 }
 
 #[inline(always)]
 pub(crate) fn set_multipan_panid(index: MultipanIndex, panid: u16) {
     unsafe {
-        let pan_id = ieee802154().inf0_pan_id.as_ptr().offset(4 * index as isize);
+        let pan_id = ieee802154()
+            .inf0_pan_id()
+            .as_ptr()
+            .offset(4 * index as isize);
         pan_id.write_volatile(panid as u32);
     }
 }
@@ -282,7 +291,7 @@ pub(crate) fn set_multipan_panid(index: MultipanIndex, panid: u16) {
 pub(crate) fn set_multipan_short_addr(index: MultipanIndex, value: u16) {
     unsafe {
         let short_addr = ieee802154()
-            .inf0_short_addr
+            .inf0_short_addr()
             .as_ptr()
             .offset(4 * index as isize);
         short_addr.write_volatile(value as u32);
@@ -293,7 +302,7 @@ pub(crate) fn set_multipan_short_addr(index: MultipanIndex, value: u16) {
 pub(crate) fn set_multipan_ext_addr(index: MultipanIndex, ext_addr: *const u8) {
     unsafe {
         let mut ext_addr_ptr = ieee802154()
-            .inf0_extend_addr0
+            .inf0_extend_addr0()
             .as_ptr()
             .offset(4 * index as isize);
 
@@ -318,28 +327,28 @@ pub(crate) fn set_multipan_ext_addr(index: MultipanIndex, ext_addr: *const u8) {
 #[inline(always)]
 pub(crate) fn set_cca_mode(cca_mode: CcaMode) {
     ieee802154()
-        .ed_scan_cfg
+        .ed_scan_cfg()
         .modify(|_, w| w.cca_mode().variant(cca_mode as u8));
 }
 
 #[inline(always)]
 pub(crate) fn set_cca_threshold(cca_threshold: i8) {
     ieee802154()
-        .ed_scan_cfg
+        .ed_scan_cfg()
         .modify(|_, w| w.cca_ed_threshold().variant(cca_threshold as u8))
 }
 
 #[inline(always)]
 pub(crate) fn set_tx_auto_ack(enable: bool) {
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .modify(|_, w| w.hw_auto_ack_tx_en().variant(enable));
 }
 
 #[inline(always)]
 pub(crate) fn get_tx_auto_ack() -> bool {
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .read()
         .hw_auto_ack_tx_en()
         .bit_is_set()
@@ -348,21 +357,21 @@ pub(crate) fn get_tx_auto_ack() -> bool {
 #[inline(always)]
 pub(crate) fn set_rx_auto_ack(enable: bool) {
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .modify(|_, w| w.hw_auto_ack_rx_en().variant(enable));
 }
 
 #[inline(always)]
 pub(crate) fn set_tx_enhance_ack(enable: bool) {
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .modify(|_, w| w.hw_enhance_ack_tx_en().variant(enable));
 }
 
 #[inline(always)]
 pub(crate) fn get_tx_enhance_ack() -> bool {
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .read()
         .hw_enhance_ack_tx_en()
         .bit_is_set()
@@ -371,49 +380,60 @@ pub(crate) fn get_tx_enhance_ack() -> bool {
 #[inline(always)]
 pub(crate) fn set_coordinator(enable: bool) {
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .modify(|_, w| w.pan_coordinator().variant(enable));
 }
 
 #[inline(always)]
 pub(crate) fn set_promiscuous(enable: bool) {
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .modify(|_, w| w.promiscuous_mode().variant(enable));
 }
 
 #[inline(always)]
 pub(crate) fn set_pending_mode(enable: bool) {
     ieee802154()
-        .ctrl_cfg
+        .ctrl_cfg()
         .modify(|_, w| w.autopend_enhance().variant(enable));
 }
 
 #[inline(always)]
 pub(crate) fn get_events() -> u16 {
-    ieee802154().event_status.read().bits() as u16
+    ieee802154().event_status().read().bits() as u16
 }
 
 #[inline(always)]
 pub(crate) fn clear_events(events: u16) {
     ieee802154()
-        .event_status
+        .event_status()
         .modify(|r, w| unsafe { w.event_status().bits(r.event_status().bits() & events) });
 }
 
 #[inline(always)]
 pub(crate) fn set_transmit_security(enable: bool) {
-    ieee802154().sec_ctrl.modify(|_, w| w.sec_en().bit(enable));
+    ieee802154()
+        .sec_ctrl()
+        .modify(|_, w| w.sec_en().bit(enable));
 }
 
 #[inline(always)]
 pub(crate) fn set_rx_addr(addr: *mut u8) {
     ieee802154()
-        .rxdma_addr
+        .rxdma_addr()
         .modify(|_, w| w.rxdma_addr().variant(addr as u32));
 }
 
 #[inline(always)]
-pub(crate) fn ieee802154() -> &'static self::ral::ieee802154::RegisterBlock {
-    unsafe { &*self::ral::IEEE802154::PTR }
+pub(crate) fn abort_tx() {
+    ieee802154()
+        .tx_status()
+        .modify(|_, w| w.tx_abort_status().variant(0));
+}
+
+#[inline(always)]
+pub(crate) fn abort_rx() {
+    ieee802154()
+        .rx_status()
+        .modify(|_, w| w.rx_abort_status().variant(0));
 }
