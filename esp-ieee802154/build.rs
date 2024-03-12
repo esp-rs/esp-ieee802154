84fb4c6cb0deedcf330f181d88cbbe0e7383599e
Björn Quentin <bjoernQ@users.noreply.github.com>
2024-03-12 18:34:49 +0100

    Use esp-hal 0.16.1 (#20)

--- esp-ieee802154/build.rs
+++ esp-ieee802154/build.rs
@@ -20,6 +20,11 @@ macro_rules! copy_file {
 fn main() {
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
+    #[cfg(not(any(feature = "esp32c6", feature = "esp32h2")))]
+    {
+        compile_error!("Either esp32c6 or esp32h2 needs to be selected via a feature");
+    }
+
     #[cfg(feature = "esp32c6")]
     {
         copy_file!(out, "ld/esp32c6/rom_coexist.x");
